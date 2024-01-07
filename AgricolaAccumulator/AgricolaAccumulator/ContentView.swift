64e5ac46c3d84bdea2c1f8c5d299de6e08cce245
Rishi Patel <rishinpatel@gmail.com>
2024-01-07 15:09:26 -0800

    add round button

--- AgricolaAccumulator/AgricolaAccumulator/ContentView.swift
+++ AgricolaAccumulator/AgricolaAccumulator/ContentView.swift
@@ -1,24 +1,72 @@
+////
+////  ContentView.swift
+////  AgricolaAccumulator
+////
+////  Created by Rishi Patel on 1/7/24.
+////
 //
-//  ContentView.swift
-//  AgricolaAccumulator
+//import SwiftUI
 //
-//  Created by Rishi Patel on 1/7/24.
+//struct ContentView: View {
+//    var body: some View {
+//        VStack {
+//            Image(systemName: "globe")
+//                .imageScale(.large)
+//                .foregroundStyle(.tint)
+//            Text("Hello, world!")
+//        }
+//        .padding()
+//    }
+//}
 //
-
+//#Preview {
+//    ContentView()
+//}
 import SwiftUI
 
 struct ContentView: View {
+    @State private var count :Int = 1
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+            
+            Text("Wood")
+            
+            Button(action: {
+                count = 0
+            }) {
+                Text("\(count)")
+                    .font(.title)
+                    .padding()
+                    .foregroundColor(.white)
+                    .background(Color.cyan)
+                    .cornerRadius(20)
+            }
+            
+//            Button(action: {count -= 1}) {
+//                Text("test")
+//            }
+            Spacer()
+            
+            Button(action: {
+                count += 1 // Incrementing count for "Next Round"
+            }) {
+                Text("Next Round")
+                    .font(.headline)
+                    .padding()
+                    .foregroundColor(.white)
+                    .background(Color.green)
+                    .cornerRadius(10)
+            }
+            
+            Spacer()
+            
+        }.padding()
     }
 }
 
-#Preview {
-    ContentView()
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
 }
