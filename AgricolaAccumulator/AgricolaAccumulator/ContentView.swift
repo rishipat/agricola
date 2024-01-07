0aec0de5faf4d09253b26c1795234f697513c3bd
Rishi Patel <rishinpatel@gmail.com>
2024-01-07 14:31:48 -0800

    start ios project

---
+++ AgricolaAccumulator/AgricolaAccumulator/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AgricolaAccumulator
+//
+//  Created by Rishi Patel on 1/7/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
